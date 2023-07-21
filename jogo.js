b1b5f9a6aac572c7029e4fb4d3d513589eecc748
anakelysantos <anasa.prof@gmail.com>
2023-07-21 16:41:59 -0300

    t

--- jogo.js
+++ jogo.js
@@ -5,7 +5,10 @@ const timer = document.querySelector('.timer')
 const modal = document.getElementById("modal");
 const closeModal = document.getElementsByClassName("close")[0];
 
-const soni = document.getElementById("texto")
+const soni = document.getElementById("question")
+const a = document.getElementById("a")
+const b = document.getElementById("b")
+var certa = 0; // Declaramos a variável certa no escopo glob
 
 const characters = [
     '1',
@@ -43,10 +46,13 @@ const checkEndGame = () => {
 }
 
 
+
+
 const checkCards = () => {
 
 const firstCharacter = firstCard.getAttribute('data-character')
 const secondCharacter = secondCard.getAttribute('data-character')
+
 
 if (firstCharacter == secondCharacter) {
     
@@ -55,48 +61,87 @@ secondCard.firstChild.classList.add('disabled-card')
 
 
 
+
+
 firstCard =''
 secondCard=''
 
 checkEndGame()
+
+
+
 const exibirModal = () => {
     modal.style.display = "block";
+
+      
     switch (firstCharacter) {
         case "1":
-            soni.innerHTML = '<p>Sônia Guimarães foi a primeira mulher brasileira a obter o título de doutora em Física.</p>';
-
+            soni.innerHTML = '<p>Foi a primeira mulher brasileira a obter o título de doutora em?</p>';
+           a.innerHTML=`fisica`;
+           b.innerHTML=`quimica`;
+           certa = 1;
             
-                
+           
             
             break;
         case "2":
-            soni.innerHTML = '<p>Em 2017, foi criado dentro do Instituto de Física da Universidade de São Paulo o Coletivo Negro Sonia Guimarães. Esse coletivo tem como objetivo principal ser um ambiente de acolhimento e apoio aos estudantes negros da instituição. Ele busca promover a equidade e a inclusão racial no âmbito acadêmico, oferecendo suporte emocional, orientação e incentivo aos estudantes negros que enfrentam desafios e enfrentam questões relacionadas à representatividade e ao acesso igualitário à educação.</p>';
+            soni.innerHTML = '<p>Qual é o objetivo do Coletivo Negro Sonia Guimarães na Universidade de São Paulo?</p>';
+            a.innerHTML=`Realizar eventos culturais na universidade.`;
+            b.innerHTML=`Promover a equidade e a inclusão racial no âmbito acadêmico, oferecendo suporte emocional e orientação aos estudantes negros.`;
+             certa = 1
             break;
         case "3":
-            soni.innerHTML = '<p>Sônia sempre se destacou academicamente, especialmente em matemática, e seu amor pela ciência a levou a cursar Física na Universidade Federal de São Carlos.</p>';
+            soni.innerHTML = '<p>Qual foi a área acadêmica em que Sônia se destacou e que a levou a cursar na Universidade Federal de São Carlos?</p>';
+            a.innerHTML=`Matemática.`;
+            b.innerHTML=` História`;
+             certa = 0
             break;
         case "4":
-            soni.innerHTML = '<p>Além de suas conquistas acadêmicas, Sônia também desempenha um papel ativo na luta pela inclusão de mulheres e negros no ambiente acadêmico. Ela é uma das fundadoras da Faculdade Zumbi dos Palmares e está envolvida em projetos que visam proporcionar oportunidades para estudantes de áreas carentes e marginalizadas.</p>';
+            soni.innerHTML = '<p>Além de suas conquistas acadêmicas, qual é o papel ativo desempenhado por Sônia?</p>';
+            a.innerHTML=`Ela é uma renomada artista de teatro.`;
+            b.innerHTML=` Luta pela inclusão de mulheres e negros no ambiente acadêmico, fundadora da Faculdade Zumbi dos Palmares e envolvida em projetos de oportunidades para estudantes de áreas carentes e marginalizadas.`;
+             certa = 1
             break;
         case "5":
-            soni.innerHTML = '<p>Ao retornar ao Brasil, Sônia Guimarães fez história ao se tornar a primeira mulher a dar aulas no Instituto Tecnológico de Aeronáutica (ITA). Ela enfrentou desafios como a falta de representatividade feminina na área de exatas, mas sua dedicação e perseverança abriram caminho para mais inclusão e diversidade nas ciências exatas.</p>';
+            soni.innerHTML = '<p>Qual foi a conquista histórica de Sônia Guimarães no Instituto Tecnológico de Aeronáutica (ITA)?</p>';
+            a.innerHTML=`Tornar-se a primeira mulher a dar aulas no ITA, abrindo caminho para mais inclusão e diversidade nas ciências exatas.`;
+           b.innerHTML=`Tornar-se a primeira mulher a dar aulas em uma escola de dança renomada.`;
+            certa = 0
             break;
         case "6":
-            soni.innerHTML = '<p>Durante sua jornada acadêmica, Sônia inicialmente planejava seguir a carreira de Engenharia Civil, mas suas aulas sobre materiais sólidos despertaram seu interesse pela Física. Após concluir a graduação, ela prosseguiu para o mestrado em Física Aplicada, onde se dedicou ao estudo da técnica elipsométrica para caracterização ótica de filmes finos.</p>';
+            soni.innerHTML = '<p>Qual foi o interesse que surgiu em Sônia Guimarães durante sua jornada acadêmica?</p>';
+            a.innerHTML=`Física, após suas aulas sobre materiais sólidos despertarem seu interesse.`;
+           b.innerHTML=` Biologia, após suas aulas de matemática.`;
+            certa = 0
             break;
         case "7":
-            soni.innerHTML = '<p>Em 1986, Sônia Guimarães teve a oportunidade de realizar um breve período de pesquisa no Istituto LAMEL do CNR em Bologna, na Itália. Essa experiência permitiu que ela expandisse seus conhecimentos e estabelecesse conexões com outros pesquisadores renomados na área. Durante sua estadia no instituto, Sônia teve a oportunidade de aprofundar seus estudos e explorar novas perspectivas em sua área de pesquisa. Essa experiência internacional contribuiu para o seu crescimento acadêmico e enriqueceu seu repertório científico, fortalecendo ainda mais sua trajetória profissional na Física.</p>';
+            soni.innerHTML = '<p>Em que país Sônia Guimarães teve a oportunidade de realizar pesquisa em 1986?</p>';
+            a.innerHTML=` França.`;
+           b.innerHTML=` Itália.`;
+            certa = 1
             break;
         case "8":
-            soni.innerHTML = '<p>O caminho de Sônia a levou além das fronteiras do Brasil, conduzindo-a a um doutorado na área de materiais eletrônicos na The University of Manchester Institute of Science and Technology, na Inglaterra. Em 1989, ela concluiu seu pós-doutorado, tornando-se a primeira mulher negra brasileira a obter esse título.</p>';
+            soni.innerHTML = '<p>Onde Sônia Guimarães concluiu seu doutorado e se tornou a primeira mulher negra brasileira a obter esse título?</p>';
+            a.innerHTML=`The University of Manchester Institute of Science and Technology, na Inglaterra.`;
+           b.innerHTML=` Universidade de São Paulo, no Brasil.`;
+            certa = 0
             break;
         case "9":
-            soni.innerHTML = '<p>Ela cresceu em meio a desafios, estudando em escolas públicas e trabalhando para investir em sua educação.</p>';
+            soni.innerHTML = '<p>Como foi a educação de Sônia Guimarães durante sua infância e adolescência?</p>';
+            a.innerHTML=` Ela estudou em escolas públicas e trabalhou para investir em sua educação.`;
+           b.innerHTML=` Ela frequentou escolas particulares e não precisou trabalhar para investir em sua educação.`;
+            certa = 0
             break;
         default:
-            soni.innerHTML = '<p>Sônia Guimarães é uma física brasileira que se tornou um marco na história da ciência ao se tornar a primeira mulher doutorada em Física no Brasil. Nascida em São Paulo, no dia 26 de junho de 1957.</p>';
+            soni.innerHTML = '<p>Qual marco histórico Sônia Guimarães alcançou na ciência?</p>';
+            a.innerHTML=`Tornou-se a primeira mulher doutorada em Física no Brasil.`;
+           b.innerHTML=`Foi a primeira mulher a receber o Prêmio Nobel em Física.`;
+            certa = 1
             break;
     }
+    
+    
+    
     
     
   }
@@ -206,4 +251,16 @@ window.onload = ()=>{
 }
 
 
+
+function cor() {
+
+    if (certa == 0) {
+        a.style.backgroundColor = "#31FF4E";
+        b.style.backgroundColor = "#FF0F0F";
+    }else{
+        b.style.backgroundColor = "#31FF4E";
+        a.style.backgroundColor = "FF0F0F";
+    }
+
+}
 
